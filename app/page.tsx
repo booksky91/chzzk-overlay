c90da2577b428762154111ec0969a1bb4537a6ae
booksky <booksky91@gmail.com>
2023-12-20 17:34:26 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
     return (
         <main style={{color: "black"}}>
             <h1>치지직 오버레이</h1>
+            <p>
+                https://github.com/kimcore/chzzk-overlay 포크해서 수정된 코드입니다.<br/>
+            </p>
         </main>
     )
 }
