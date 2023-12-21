f39470263fb9dc63d7dd510f2c1197a9268f1e6c
booksky <booksky91@gmail.com>
2023-12-21 17:30:40 +0900

    Update chat.tsx

--- app/[channelId]/chat.tsx
+++ app/[channelId]/chat.tsx
@@ -8,20 +8,19 @@ import {ChatEvent, ChzzkChat} from "chzzk"
 const emojiRegex = /{:([a-zA-Z0-9_]+):}/g
 
 export default function Chat({chatChannelId, accessToken}) {
-    const [chats, setChats] = useState([]);
-    const [chatClasses, setChatClasses] = useState([]);
+    const [chats, setChats] = useState([])
 
-    const searchParams = useSearchParams();
-    const small = searchParams.has("small");
+    const searchParams = useSearchParams()
+    const small = searchParams.has("small")
 
     function onChat(chat: ChatEvent) {
-        const id = `${chat.profile.userIdHash}-${chat.time}`;
-        const nickname = chat.profile.nickname;
+        const id = `${chat.profile.userIdHash}-${chat.time}`
+        const nickname = chat.profile.nickname
         const badges = chat.profile.activityBadges
             ?.filter(badge => badge.activated)
-            ?.map(badge => ({name: badge.title, src: badge.imageUrl})) || [];
-        const emojis = chat.extras.emojis || {};
-        const message = chat.message;
+            ?.map(badge => ({name: badge.title, src: badge.imageUrl})) || []
+        const emojis = chat.extras.emojis || {}
+        const message = chat.message
 
         setChats((prevState) => {
             const newChats = prevState.concat([{
@@ -30,63 +29,57 @@ export default function Chat({chatChannelId, accessToken}) {
                 nickname,
                 emojis,
                 message
-            }]);
+            }])
 
             if (newChats.length > 50) {
-                newChats.splice(0, newChats.length - 50);
+                newChats.splice(0, newChats.length - 50)
             }
 
-            return newChats;
-        });
-
-        setChatClasses((prevState) => {
-            const isEvenChat = (prevState.length % 2 === 0);
-            return prevState.concat([isEvenChat ? "even" : "odd"]);
-        });
+            return newChats
+        })
     }
 
     useEffect(() => {
-        const chzzkChat = ChzzkChat.fromAccessToken(chatChannelId, accessToken);
-        chzzkChat.on("chat", onChat.bind(this));
-        chzzkChat.on("connect", () => chzzkChat.requestRecentChat(50));
-        chzzkChat.connect();
-    }, []);
+        const chzzkChat = ChzzkChat.fromAccessToken(chatChannelId, accessToken)
+        chzzkChat.on("chat", onChat.bind(this))
+        chzzkChat.on("connect", () => chzzkChat.requestRecentChat(50))
+        chzzkChat.connect()
+    }, [])
 
     useEffect(() => {
-        window.scrollTo(0, document.body.scrollHeight);
-    }, [chats]);
+        window.scrollTo(0, document.body.scrollHeight)
+    }, [chats])
 
     return (
         <div id="log" className={clsx(small && "small")}>
-            {chats.map((chat, index) => {
-                const match = chat.message.match(emojiRegex);
-                const chatClass = chatClasses[index];
+            {chats.map(chat => {
+                const match = chat.message.match(emojiRegex)
 
                 return (
-                    <div key={chat.id} className={clsx(chatClass)} data-from={chat.nickname}>
+                    <div key={chat.id} data-from={chat.nickname}>
                         <span className="message">
-                            {match ? (
-                                <Fragment>
-                                    {chat.message.split(emojiRegex).map((part, i) => {
-                                        if (i % 2 === 0) {
-                                            return part;
-                                        } else {
-                                            const src = chat.emojis[part];
-                                            return (
-                                                <span key={i} className="emote_wrap">
-                                                    <img alt={`{:${part}:}`} className="emoticon" src={src}/>
-                                                </span>
-                                            );
-                                        }
-                                    })}
-                                </Fragment>
-                            ) : (
-                                chat.message
-                            )}
-                        </span>
+                                {match ? (
+                                    <Fragment>
+                                        {chat.message.split(emojiRegex).map((part: string, i: number) => {
+                                            if (i % 2 == 0) {
+                                                return part
+                                            } else {
+                                                const src = chat.emojis[part]
+                                                return (
+                                                    <span key={i} className="emote_wrap">
+                                                        <img alt={`{:${part}:}`} className="emoticon" src={src}/>
+                                                    </span>
+                                                )
+                                            }
+                                        })}
+                                    </Fragment>
+                                ) : (
+                                    chat.message
+                                )}
+                            </span>
                     </div>
-                );
+                )
             })}
         </div>
-    );
+    )
 }
