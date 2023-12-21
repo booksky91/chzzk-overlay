3a6d900218b12590885278343a9488c38f623d4d
booksky <booksky91@gmail.com>
2023-12-21 22:21:05 +0900

    Update page.tsx

--- app/[channelId]/page.tsx
+++ app/[channelId]/page.tsx
@@ -1,15 +1,24 @@
-import Chat from "@/app/[channelId]/chat"
+import ChatBox from "./chat"
+import {notFound} from "next/navigation"
+
+export const dynamic = "force-dynamic"
 
 export default async function ChatPage({params: {channelId}}) {
-    const chatChannelId = await fetch(`https://api.chzzk.naver.com/polling/v1/channels/${channelId}/live-status`)
-        .then(r => r.json())
-        .then(data => data['content']['chatChannelId'])
+    const {signal} = new AbortController()
 
-    const accessToken = await fetch(`https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId=${chatChannelId}&chatType=STREAMING`)
-        .then(r => r.json())
-        .then(data => data['content']['accessToken'])
+    const chatChannelId = await fetch(
+        `https://api.chzzk.naver.com/polling/v1/channels/${channelId}/live-status`,
+        {signal}
+    ).then(r => r.json()).then(data => data['content']?.['chatChannelId'])
+
+    if (!chatChannelId) return notFound()
+
+    const accessToken = await fetch(
+        `https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId=${chatChannelId}&chatType=STREAMING`,
+        {signal}
+    ).then(r => r.json()).then(data => data['content']['accessToken'])
 
     return (
-        <Chat chatChannelId={chatChannelId} accessToken={accessToken}/>
+        <ChatBox chatChannelId={chatChannelId} accessToken={accessToken}/>
     )
 }
