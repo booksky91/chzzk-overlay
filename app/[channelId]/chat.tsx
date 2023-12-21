0de0b7dcc5bfdbd2c46f044d4c4694ab7ee33ac3
booksky <booksky91@gmail.com>
2023-12-21 18:28:33 +0900

    Update chat.tsx

--- app/[channelId]/chat.tsx
+++ app/[channelId]/chat.tsx
@@ -1,90 +1,89 @@
 "use client"
 
-import { Fragment, useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { clsx } from "clsx";
-import { ChatEvent, ChzzkChat } from "chzzk";
+import {Fragment, useEffect, useState} from "react"
+import {useSearchParams} from "next/navigation"
+import {clsx} from "clsx"
+import {ChatEvent, ChzzkChat} from "chzzk"
 
-const emojiRegex = /{:([a-zA-Z0-9_]+):}/g;
+const emojiRegex = /{:([a-zA-Z0-9_]+):}/g
 
-export default function Chat({ chatChannelId, accessToken }) {
-  const [chats, setChats] = useState([]);
-  const searchParams = useSearchParams();
-  const small = searchParams.has("small");
+export default function Chat({chatChannelId, accessToken}) {
+    const [chats, setChats] = useState([])
+    const [classOrder, setClassOrder] = useState(1) // Added state for class order
 
-  function onChat(chat: ChatEvent) {
-    setChats((prevChats) => {
-      const id = `${chat.profile.userIdHash}-${chat.time}`;
-      const nickname = chat.profile.nickname;
-      const badges = chat.profile.activityBadges
-        ?.filter((badge) => badge.activated)
-        ?.map((badge) => ({ name: badge.title, src: badge.imageUrl })) || [];
-      const emojis = chat.extras.emojis || {};
-      const message = chat.message;
+    const searchParams = useSearchParams()
+    const small = searchParams.has("small")
 
-      const newChats = prevChats.concat([
-        {
-          id,
-          badges,
-          nickname,
-          emojis,
-          message,
-        },
-      ]);
+    function onChat(chat: ChatEvent) {
+        const id = `${chat.profile.userIdHash}-${chat.time}`
+        const nickname = chat.profile.nickname
+        const badges = chat.profile.activityBadges
+            ?.filter(badge => badge.activated)
+            ?.map(badge => ({name: badge.title, src: badge.imageUrl})) || []
+        const emojis = chat.extras.emojis || {}
+        const message = chat.message
 
-      if (newChats.length > 50) {
-        newChats.splice(0, newChats.length - 50);
-      }
+        setChats((prevState) => {
+            const newChats = prevState.concat([{
+                id,
+                badges,
+                nickname,
+                emojis,
+                message,
+                classOrder // Assign class order to each chat
+            }])
 
-      return newChats;
-    });
-  }
+            if (newChats.length > 50) {
+                newChats.splice(0, newChats.length - 50)
+            }
 
-  useEffect(() => {
-    const chzzkChat = ChzzkChat.fromAccessToken(chatChannelId, accessToken);
-    chzzkChat.on("chat", onChat.bind(this));
-    chzzkChat.on("connect", () => chzzkChat.requestRecentChat(50));
-    chzzkChat.connect();
-  }, []);
+            setClassOrder((prevOrder) => prevOrder + 1); // Increment class order
 
-  useEffect(() => {
-    window.scrollTo(0, document.body.scrollHeight);
-  }, [chats]);
+            return newChats
+        })
+    }
 
-  return (
-    <div id="log" className={clsx(small && "small")}>
-      {chats.map((chat, index) => {
-        const match = chat.message.match(emojiRegex);
+    useEffect(() => {
+        const chzzkChat = ChzzkChat.fromAccessToken(chatChannelId, accessToken)
+        chzzkChat.on("chat", onChat.bind(this))
+        chzzkChat.on("connect", () => chzzkChat.requestRecentChat(50))
+        chzzkChat.connect()
+    }, [])
 
-        return (
-          <div
-            key={chat.id}
-            data-from={chat.nickname}
-            className={`chat-order-${index + 1}`} // Add class based on chat order
-          >
-            <span className="message">
-              {match ? (
-                <Fragment>
-                  {chat.message.split(emojiRegex).map((part: string, i: number) => {
-                    if (i % 2 == 0) {
-                      return part;
-                    } else {
-                      const src = chat.emojis[part];
-                      return (
-                        <span key={i} className="emote_wrap">
-                          <img alt={`{:${part}:}`} className="emoticon" src={src} />
+    useEffect(() => {
+        window.scrollTo(0, document.body.scrollHeight)
+    }, [chats])
+
+    return (
+        <div id="log" className={clsx(small && "small")}>
+            {chats.map(chat => {
+                const match = chat.message.match(emojiRegex);
+
+                return (
+                    <div key={chat.id} data-from={chat.nickname} className={`chat-order-${chat.classOrder}`}>
+                        <span className="message">
+                            {match ? (
+                                <Fragment>
+                                    {chat.message.split(emojiRegex).map((part: string, i: number) => {
+                                        if (i % 2 == 0) {
+                                            return part
+                                        } else {
+                                            const src = chat.emojis[part]
+                                            return (
+                                                <span key={i} className="emote_wrap">
+                                                    <img alt={`{:${part}:}`} className="emoticon" src={src}/>
+                                                </span>
+                                            )
+                                        }
+                                    })}
+                                </Fragment>
+                            ) : (
+                                chat.message
+                            )}
                         </span>
-                      );
-                    }
-                  })}
-                </Fragment>
-              ) : (
-                chat.message
-              )}
-            </span>
-          </div>
-        );
-      })}
-    </div>
-  );
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
