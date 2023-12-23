a7c7711b1f8fcb1e1e900511aa744de78f3ddfb9
booksky <booksky91@gmail.com>
2023-12-23 19:14:01 +0900

    Update ChatRow.tsx

--- app/[channelId]/ChatRow.tsx
+++ app/[channelId]/ChatRow.tsx
@@ -20,7 +20,7 @@ function ChatRow(props: Chat) {
     const match = message.match(emojiRegex)
 
     return (
-        <div className={clsx(order % 2 === 0 && "even", order % 2 !== 0 && "odd")}>
+        <div className={order}>
             <span className="message">
                 {match ? message.split(emojiRegex).map((part, i) => (
                     <Fragment key={i}>
