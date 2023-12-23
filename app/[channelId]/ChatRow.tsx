4528b97447668675f01fa709a56254d2073983ca
booksky <booksky91@gmail.com>
2023-12-23 18:29:52 +0900

    Update ChatRow.tsx

--- app/[channelId]/ChatRow.tsx
+++ app/[channelId]/ChatRow.tsx
@@ -14,6 +14,14 @@ export interface Chat {
     message: string;
 }
 
+export const nicknameColors = [
+    "#ECA843", "#EEA05D", "#EA723D", "#EAA35F", "#E98158", "#E97F58", "#E76D53", "#E66D5F", "#E56B79", "#E16490",
+    "#E481AE", "#E68199", "#DC5E9A", "#E16CB5", "#D25FAC", "#D263AE", "#D66CB4", "#D071B6", "#BA82BE", "#AF71B5",
+    "#A96BB2", "#905FAA", "#B38BC2", "#9D78B8", "#8D7AB8", "#7F68AE", "#9F99C8", "#717DC6", "#5E7DCC", "#5A90C0",
+    "#628DCC", "#7994D0", "#81A1CA", "#ADD2DE", "#80BDD3", "#83C5D6", "#8BC8CB", "#91CBC6", "#83C3BB", "#7DBFB2",
+    "#AAD6C2", "#84C194", "#B3DBB4", "#92C896", "#94C994", "#9FCE8E", "#A6D293", "#ABD373", "#BFDE73", "#CCE57D"
+]
+
 const emojiRegex = /{:([a-zA-Z0-9_]+):}/g
 
 function ChatRow(props: Chat) {
@@ -22,6 +30,13 @@ function ChatRow(props: Chat) {
 
     return (
         <div data-from={nickname}>
+            <span className="meta" style={{ color: typeof color == "number" ? nicknameColors[color] : color }}>
+                {badges.map(({name, src}, i) => (
+                    <img key={i} className="badge" alt={name} src={src} />
+                ))}
+                <span className="name">{nickname}</span>
+                <span className="colon">:</span>
+            </span>
             <span className="message">
                 {match ? message.split(emojiRegex).map((part, i) => (
                     <Fragment key={i}>
@@ -34,5 +49,4 @@ function ChatRow(props: Chat) {
         </div>
     )
 }
-
 export default memo(ChatRow)
